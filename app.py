8ccc81bee80cc5200ff129d3abb0990466222726
Suneilkumar <suneilkumar@gmail.com>
2025-10-28 10:02:37 +0530

    add cors

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import json
 # App setup
 # -------------------------------------------------------
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": ["https://main.d2fa862e1himfr.amplifyapp.com"]}})
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(BASE_DIR, 'questions.db')}"
@@ -170,4 +170,4 @@ def search_questions():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=5000)
