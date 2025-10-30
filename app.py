e1d1dda0f7dcde24d1fb40621492faa5ee6a8d66
Suneilkumar <suneilkumar@gmail.com>
2025-10-30 12:59:16 +0530

    no

--- app.py
+++ app.py
@@ -374,8 +374,9 @@ def quiz_summary():
         day["user_id"] = r.user_id
         day["email"] = r.email
         day["date"] = date
-        current_time = time or "00:00:00"
-        last_time = day.get("last_attempt_time", "00:00:00")
+        current_time = str(time or "00:00:00")
+        last_time = str(day.get("last_attempt_time") or "00:00:00")
+
         day["last_attempt_time"] = max(current_time, last_time)
         day["total_attempts"] += 1
         day["total_questions"] += 1
